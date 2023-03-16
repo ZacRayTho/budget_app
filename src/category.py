8398f793a59640c872f2c033ac45608bfe21268d
ZacRayTho <zachary.thomas@student.awesomeinc.org>
2023-03-16 14:01:36 +0000

    got base project built

---
+++ src/category.py
@@ -0,0 +1,21 @@
+class Category():
+    def __init__(self, name):
+        self.name = name
+        ledger = []
+        
+        pass
+
+    def deposit(self):
+        pass
+
+    def withdraw(self):
+        pass
+
+    def get_balance(self):
+        pass
+
+    def transfer(self):
+        pass
+
+    def check_funds(self):
+        pass
